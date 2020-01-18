9bb1cbd342c859407d87af12276254925443a58f
muhridlo <59998833+muhridlo@users.noreply.github.com>
2020-01-18 20:23:00 +0700

    Update polinomial.c

--- polinomial.c
+++ polinomial.c
@@ -27,3 +27,71 @@ int penjumlahan (){
 	p0 = b+d;
 	
 	printf("Hasilnya adalah %dx^2 + %dx + %d ",p2,p1,p0);
+
+int pengurangan (){
+	int a,b,c,d ;
+	int p2,p1,p0 ;
+	
+	printf("\nPengurangan \n");
+	printf("Masukkan a: ");
+	scanf("%d" , &a);
+	printf("Masukkan b: ");
+	scanf("%d" , &b);
+	printf("Masukkan c: ");
+	scanf("%d" , &c);
+	printf("Masukkan d: ");
+	scanf("%d" , &d);
+	
+	p2 = a;
+	p1 = -c;
+	p0 = b-d;
+	
+	printf("Hasilnya adalah %dx^2 + (%dx)+ (%d)", p2, p1,p0);
+}
+int perkalian (){
+	int a,b,c,d ;
+	int p3,p2,p1,p0 ;
+	
+	printf("\nPerkalian \n");
+	printf("Masukkan a: ");
+	scanf("%d" , &a);
+	printf("Masukkan b: ");
+	scanf("%d" , &b);
+	printf("Masukkan c: ");
+	scanf("%d" , &c);
+	printf("Masukkan d: ");
+	scanf("%d" , &d);
+	
+	p3 = a*c;
+	p2 = a*d;
+	p1 = b*c;
+	p0 = b*d;
+	
+	printf("Hasilnya adalah %dx^3 + %dx^2 + %dx + %d ", p2,p1,p0);
+}
+
+int turunan (){
+	int a,b,c,d,e ;
+	int p4,p3,p2,p1,p0 ;
+	
+	printf("\nTurunan dari ax^4 + bx^3 + cx^2 + dx + e");
+	printf("\nTurunan \n");
+	printf("Masukkan a: ");
+	scanf("%d" , &a);
+	printf("Masukkan b: ");
+	scanf("%d" , &b);
+	printf("Masukkan c: ");
+	scanf("%d" , &c);
+	printf("Masukkan d: ");
+	scanf("%d" , &d);
+	printf("Masukkan e: ");
+	scanf("%d" , &e);
+	
+	p4 = a*4;
+	p3 = b*3;
+	p2 = c*2;
+	p1 = d*1;
+	p0 = e*0;
+	
+	printf("Hasilnya adalah %d x^3 + %d x^2 + %d x + %d + %d ", p4,p3,p2,p1,p0);
+}
